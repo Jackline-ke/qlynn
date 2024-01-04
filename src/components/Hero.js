0065bec4d1f0cc9b4fae14bd20c17d28f2e6e2d8
Jackline-ke <antoninahodhiambo@gmail.com>
2024-01-04 16:49:23 +0300

    files

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import {FaTwitter, FaLinkedin, FaGithub, FaMedium} from 'react-icons/fa'
 import {Link} from 'react-scroll'
+import wave from '../assets/wave.png'
 
 const Hero = () => {
   return (
     <div className='w-full h-screen bg-[#f9f2ec] text-[#4d3319] font-serif flex justify-center items-center px-4'>
         <div className='text-center py-8'>
-            <p className='text-lg'>Hi there! <br/> <span className='text-5xl sm:text-6xl font-semibold'>I'm Jackline Adhiambo</span></p>
+            <div className='flex items-center justify-center'>
+              <p>Hi there!</p>
+              <img src={wave} className='w-16'/>
+            </div>
+            <p className='text-5xl sm:text-6xl font-semibold'>I'm Jackline Adhiambo</p>
             <p className="px-4 mb-3 max-w-lg mx-auto lg:mx-0 text-lg sm:text-xl">A Frontend Developer and a UI/UX Designer who designs and implement high end web application.</p>
             <Link><p className="text-2xl font-bold">My Resume</p></Link>
             <div className='flex gap-4 justify-center mt-5'>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,7 +8,7 @@ const Projects = () => {
         <div className='grid grid-cols-1 lg:grid-cols-2 gap-6'>
             <div className='bg-[#f9f2ec] p-6 rounded-md'>
                 <div className='flex flex-col gap-2'>
-                    <img src={market} className="rounded"/>
+                    <img src={market} alt='project' className="rounded"/>
                     <h2 className='text-xl font-bold'>marketHub</h2>
                     <p className=''>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                     <h3 className=''><span className='font-bold'>Stack/Tools:</span> React, Tailwind</h3>
@@ -16,7 +16,7 @@ const Projects = () => {
             </div>
             <div className='bg-[#f9f2ec] p-8 rounded-md'>
                 <div className='flex flex-col gap-2'>
-                    <img src={market} className="rounded"/>
+                    <img src={market} alt='project' className="rounded"/>
                     <h2 className='text-xl font-bold'>marketHub</h2>
                     <p className=''>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                     <h3 className=''><span className='font-bold'>Stack/Tools:</span> React, Tailwind</h3>
@@ -24,7 +24,7 @@ const Projects = () => {
             </div>
             <div className='bg-[#f9f2ec] p-8 rounded-md'>
                 <div className='flex flex-col gap-2'>
-                    <img src={market} className="rounded"/>
+                    <img src={market} alt='project' className="rounded"/>
                     <h2 className='text-xl font-bold'>marketHub</h2>
                     <p className=''>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                     <h3 className=''><span className='font-bold'>Stack/Tools:</span> React, Tailwind</h3>
