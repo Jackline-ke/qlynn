6b6d98d27cab66332396302d6a90ffd6dfaeebd1
Jackline-ke <antoninahodhiambo@gmail.com>
2024-01-05 10:54:38 +0300

    files

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,25 +9,25 @@ export const Navbar = () => {
     }
     const closeNav = () => {
         setNav(false);
-    }
+    } 
   return (
     <div className='fixed w-full px-8 py-4 md:px-16 text-[#4d3319] font-serif bg-opacity-70 shadow-md' style={{ backdropFilter: 'blur(5px)', webkitBackdropFilter: 'blur(5px)', zIndex: 1000 /* Set a higher z-index */ }}>
         <div className='flex justify-between items-center'>
             <h1>Qlynn</h1>
             <div className='hidden lg:flex gap-8 cursor-pointer'>
-                <Link to='home' smooth={true} duration={1000}>
+                <Link to='home' smooth={true} duration={2000}>
                     <h2>Home</h2>
                 </Link>
-                <Link to='features' smooth={true} duration={1000}>
+                <Link to='features' smooth={true} duration={2000}>
                     <h2>Features</h2>
                 </Link>
-                <Link to='skills' smooth={true} duration={1000}>
+                <Link to='skills' smooth={true} duration={2000}>
                     <h2>Skills</h2>
                 </Link>
-                <Link to='projects' smooth={true} duration={1000}>
+                <Link to='projects' smooth={true} duration={2000}>
                     <h2>Projects</h2>
                 </Link>
-                <Link to='contact' smooth={true} duration={1000}>
+                <Link to='contact' smooth={true} duration={2000}>
                     <h2>Contact</h2>
                 </Link>
             </div>
@@ -38,19 +38,19 @@ export const Navbar = () => {
             {/* mobile */}
             <div onClick={handleNav} className={nav ? 'overflow-hidden lg:hidden ease-in duration-300 absolute text-[#f9f2ec] top-0 right-0  w-[40%] bg-[#4d3319]/90 px-4 py-16' : 'absolute left-[-100%] top-0 h-screen ease-in duration-500' }>
                 <div  className='flex flex-col gap-4 cursor-pointer text-lg'>
-                <Link to='home' smooth={true} duration={500} onClick={closeNav}>
+                <Link to='home' smooth={true} duration={2000} onClick={closeNav}>
                     <h2>Home</h2>
                 </Link>
-                <Link to='features' smooth={true} duration={500} onClick={closeNav}>
+                <Link to='features' smooth={true} duration={2000} onClick={closeNav}>
                     <h2>Features</h2>
                 </Link>
-                <Link to='skills' smooth={true} duration={500} onClick={closeNav}>
+                <Link to='skills' smooth={true} duration={2000} onClick={closeNav}>
                     <h2>Skills</h2>
                 </Link>
-                <Link to='projects' smooth={true} duration={500} onClick={closeNav}>
+                <Link to='projects' smooth={true} duration={2000} onClick={closeNav}>
                     <h2>Projects</h2>
                 </Link>
-                <Link to='contact' smooth={true} duration={500} onClick={closeNav}>
+                <Link to='contact' smooth={true} duration={2000} onClick={closeNav}>
                     <h2>Contact</h2>
                 </Link>
                 </div>
