b181a7eeac80b038884092ff273ccc60d219a657
Jackline-ke <antoninahodhiambo@gmail.com>
2024-01-06 20:43:00 +0300

    files

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,9 +5,12 @@ const Contact = () => {
     <div id='contact' className='py-24 lg:py-0 lg:pt-0 w-full h-screen bg-[#f9f2ec] text-[#4d3319] font-serif flex flex-col justify-center items-center px-8'>
        <h3 className='mb-8 text-4xl'>Get in <span className='font-bold'>Touch!</span></h3>
        <form className='px-2 py-4 flex flex-col gap-4'>
-        <input placeholder='Your Name' className='py-4 pl-4 pr-32 text-left'/>
-        <input placeholder='Your Email' className='py-4 pl-4 pr-32'/>
-        <input placeholder='Message...' className='py-16 pl-4 pr-32'/>
+       <label>Your Name</label>
+        <input className='py-2 pl-4 pr-32 text-left'/>
+        <label>Your Email</label>
+        <input className='py-2 pl-4 pr-32'/>
+        <label>Meessage</label>
+        <input className='py-16 pl-4 pr-32'/>
       </form> 
       <button className='bg-[#4d3319] text-[#f9f2ec] mt-4 px-10 py-2 rounded-md shadow-md shadow-white'>Submit</button>
     </div>
